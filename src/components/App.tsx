4ecec9eecd8133715c49d53025d4729ec7ec6dbe
Luke Tucker <46138241+fuego161@users.noreply.github.com>
2022-04-13 00:43:07 +0100

    Use interfaces for brewRatio and Bloom

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+interface BrewRatio {
+	coffee: number;
+	water: number;
+}
+
+interface Bloom {
+	recommended: number;
+	maximum: number;
+}
+
 export const App = (): JSX.Element => {
 	// Set the brew ratio - 60g/L
-	const brewRatio = {
+	const brewRatio: BrewRatio = {
 		coffee: 15,
 		water: 250,
 	};
@@ -14,8 +24,11 @@ export const App = (): JSX.Element => {
 	// Set the brewWeight using the brewRatio
 	const brewWeight = Math.floor((brewRatio.water / brewRatio.coffee) * coffeeWeight);
 
-	// Set bloom water to two times the coffeeWeight
-	const bloomWater = coffeeWeight * 2;
+	// Set bloom water values
+	const bloom: Bloom = {
+		recommended: coffeeWeight * 2,
+		maximum: coffeeWeight * 3,
+	};
 
 	// Set the 60% pour value
 	const sixtyPercentPour = (60 / 100) * brewWeight;
