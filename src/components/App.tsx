a92e0917be5bfbe60758b87db8956f909ffc4f3f
Luke Tucker <46138241+fuego161@users.noreply.github.com>
2022-04-13 11:24:30 +0100

    Make Preparation a component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Preparation } from '../stages/Preparation';
 
 interface BrewRatio {
 	coffee: number;
@@ -69,19 +70,7 @@ export const App = (): JSX.Element => {
 		<div>
 			<h1>James Hoffmann&apos;s Ultimate V60 Technique</h1>
 
-			<div className="stage">
-				<h2 className="stage__title">Preparation</h2>
-
-				<ul className="stage__list">
-					<li className="stage__item">Grind {coffeeWeight}g of coffee</li>
-					<li className="stage__item">Rinse your paper filter with water just off the boil</li>
-					<li className="stage__item stage__item--info">This removes any paper taste and preheats the brewer</li>
-					<li className="stage__item">Add your coffee grounds to the V60</li>
-					<li className="stage__item">Create a well in the middle of the grounds</li>
-					<li className="stage__item stage__item--info">This helps to evenly saturate all the grounds during the blooming phase</li>
-					<li className="stage__item">Start timer</li>
-				</ul>
-			</div>
+			<Preparation coffeeWeight={coffeeWeight} />
 
 			<div className="stage">
 				<h2 className="stage__title">Bloom</h2>
---
+++ src/stages/Preparation.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface PreparationProps {
+	coffeeWeight: number;
+}
+
+export const Preparation = ({ coffeeWeight }: PreparationProps): JSX.Element => {
+	return (
+		<div className="stage">
+			<h2 className="stage__title">Preparation</h2>
+
+			<ul className="stage__list">
+				<li className="stage__item">Grind {coffeeWeight}g of coffee</li>
+				<li className="stage__item">Rinse your paper filter with water just off the boil</li>
+				<li className="stage__item stage__item--info">This removes any paper taste and preheats the brewer</li>
+				<li className="stage__item">Add your coffee grounds to the V60</li>
+				<li className="stage__item">Create a well in the middle of the grounds</li>
+				<li className="stage__item stage__item--info">This helps to evenly saturate all the grounds during the blooming phase</li>
+				<li className="stage__item">Start timer</li>
+			</ul>
+		</div>
+	);
+};
