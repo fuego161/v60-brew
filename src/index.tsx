ecb5d4c879a01e66eb0edb16eecb0864e34a0317
Luke Tucker <46138241+fuego161@users.noreply.github.com>
2022-04-19 19:51:37 +0100

    Move SCSS folder to assets

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './scss/index.scss';
+import './assets/scss/index.scss';
 import { App } from './components/App';
 
 const container = document.getElementById('root') as HTMLElement;
