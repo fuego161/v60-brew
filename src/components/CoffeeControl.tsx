5a3100b7b4b4ee588d8512b2db8f3b129e7049f4
Luke Tucker <46138241+fuego161@users.noreply.github.com>
2022-04-19 19:47:47 +0100

    Start Coffee Control styles

--- src/components/CoffeeControl.tsx
+++ src/components/CoffeeControl.tsx
@@ -8,25 +8,30 @@ interface CoffeeControlProps {
 
 export const CoffeeControl = ({ defaultCoffeeWeight, brewWeight, handleCoffeeChange }: CoffeeControlProps): JSX.Element => {
 	return (
-		<form className="coffeecontrol">
-			<div className="coffeecontrol__selector">
-				<label className="coffeecontrol__label" htmlFor="coffee">
+		<form className="coffee">
+			<div className="coffee__selector">
+				<label className="coffee__label" htmlFor="coffee">
 					Coffee
 				</label>
-				<input
-					id="coffee"
-					className="coffeecontrol__input"
-					name="coffee"
-					type="number"
-					min="6"
-					max="60"
-					defaultValue={defaultCoffeeWeight}
-					onChange={(e) => handleCoffeeChange(e.currentTarget)}
-				/>
-				g
+
+				<div className="coffee__controls">
+					<input
+						id="coffee"
+						className="coffee__input"
+						name="coffee"
+						type="number"
+						min="6"
+						max="60"
+						defaultValue={defaultCoffeeWeight}
+						onChange={(e) => handleCoffeeChange(e.currentTarget)}
+					/>
+					g
+				</div>
 			</div>
 
-			<div className="coffeecontrol__output">Your total brew weight will be {brewWeight}g</div>
+			<div className="coffee__output">
+				Your total brew weight will be <strong>{brewWeight}g</strong>
+			</div>
 		</form>
 	);
 };
