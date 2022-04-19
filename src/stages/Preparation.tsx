4b4572cd30e8731cafbc4981e56dc582720e37b6
Luke Tucker <46138241+fuego161@users.noreply.github.com>
2022-04-19 16:31:28 +0100

    Convert markup output to functional method

--- src/stages/Preparation.tsx
+++ src/stages/Preparation.tsx
@@ -4,22 +4,110 @@ interface PreparationProps {
 	coffeeWeight: number;
 }
 
-export const Preparation = ({ coffeeWeight }: PreparationProps): JSX.Element => {
+interface Timer {
+	present: boolean;
+	time?: string;
+}
+
+interface Information {
+	present: boolean;
+	copy?: string | JSX.Element;
+}
+
+interface Instruction {
+	copy: string | JSX.Element;
+	information: Information;
+}
+
+interface StageContent {
+	count: string;
+	title: string;
+	timer: Timer;
+	instructions: Array<Instruction>;
+}
+
+const outputStageInstructions = (timer: Timer, instructions: Array<Instruction>): JSX.Element => {
+	const instructionList: Array<JSX.Element> = [];
+
+	if (timer.present && timer.time) {
+		instructionList.push(
+			<li className="stage__item stage__item--timer" key="timer">
+				{timer.time}
+			</li>
+		);
+	}
+
+	for (const [index, instruction] of instructions.entries()) {
+		instructionList.push(
+			<li className="stage__item" key={index}>
+				{instruction.copy} {instruction.information.present && <span className="stage__information">{instruction.information.copy}</span>}
+			</li>
+		);
+	}
+
+	return <ul className="stage__list">{instructionList}</ul>;
+};
+
+const outputStageContent = (stage: StageContent): JSX.Element => {
+	const instructionList = outputStageInstructions(stage.timer, stage.instructions);
+
 	return (
 		<div className="stage">
-			<h2 className="stage__title">Preparation</h2>
+			<h3 className="stage__count">Stage {stage.count}</h3>
 
-			<ul className="stage__list">
-				<li className="stage__item">
-					Grind <strong>{coffeeWeight}g</strong> of coffee
-				</li>
-				<li className="stage__item">Rinse your paper filter with water just off the boil</li>
-				<li className="stage__item stage__item--info">This removes any paper taste and preheats the brewer</li>
-				<li className="stage__item">Add your coffee grounds to the V60</li>
-				<li className="stage__item">Create a well in the middle of the grounds</li>
-				<li className="stage__item stage__item--info">This helps to evenly saturate all the grounds during the blooming phase</li>
-				<li className="stage__item">Start timer</li>
-			</ul>
+			<h2 className="stage__title">{stage.title}</h2>
+
+			{instructionList}
 		</div>
 	);
+};
+
+export const Preparation = ({ coffeeWeight }: PreparationProps): JSX.Element => {
+	const stage: StageContent = {
+		count: 'one',
+		title: 'Preparation',
+		timer: {
+			present: false,
+		},
+		instructions: [
+			{
+				copy: (
+					<>
+						Grind <strong>{coffeeWeight}g</strong> of coffee
+					</>
+				),
+				information: {
+					present: false,
+				},
+			},
+			{
+				copy: 'Rinse your paper filter with water just off the boil',
+				information: {
+					present: true,
+					copy: 'This removes any paper taste and preheats the brewer',
+				},
+			},
+			{
+				copy: 'Add your coffee grounds to the V60',
+				information: {
+					present: false,
+				},
+			},
+			{
+				copy: 'Create a well in the middle of the grounds',
+				information: {
+					present: true,
+					copy: 'This helps to evenly saturate all the grounds during the blooming phase',
+				},
+			},
+			{
+				copy: 'Start timer',
+				information: {
+					present: false,
+				},
+			},
+		],
+	};
+
+	return outputStageContent(stage);
 };
