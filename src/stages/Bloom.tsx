a60b725ec6ac3d1e23c27ebda0a7dae9e62a0a36
Luke Tucker <46138241+fuego161@users.noreply.github.com>
2022-04-19 16:58:42 +0100

    Update Bloom stage to use StageOutput

--- src/stages/Bloom.tsx
+++ src/stages/Bloom.tsx
@@ -1,28 +1,50 @@
 import React from 'react';
+import { StageOutput } from '../utils';
 
 interface BloomProps {
 	bloom: Bloom;
 }
 
 export const Bloom = ({ bloom }: BloomProps): JSX.Element => {
-	return (
-		<div className="stage">
-			<h2 className="stage__title">Bloom</h2>
+	const stage: StageContent = {
+		count: 'two',
+		title: 'Bloom',
+		timer: {
+			present: true,
+			time: '0:00',
+		},
+		instructions: [
+			{
+				copy: (
+					<>
+						Add <strong>{bloom.recommended}g</strong> of bloom water
+					</>
+				),
+				information: {
+					present: true,
+					copy: (
+						<>
+							Do not pour more than three times the amount of coffee used (<strong>{bloom.maximum}g</strong>)
+						</>
+					),
+				},
+			},
+			{
+				copy: 'Swirl the coffee slurry until evenly mixed',
+				information: {
+					present: true,
+					copy: 'The aim is to wet all the coffee grounds by evenly mixing bloom water and coffee',
+				},
+			},
+			{
+				copy: 'Bloom for up to 45s',
+				information: {
+					present: true,
+					copy: 'This allows CO2 to escape which will improve extraction',
+				},
+			},
+		],
+	};
 
-			<p className="stage__time">Timer - 0:00</p>
-
-			<ul className="stage__list">
-				<li className="stage__item">
-					Add <strong>{bloom.recommended}g</strong> of bloom water
-				</li>
-				<li className="stage__item stage__item--info">
-					Do not pour more than three times the amount of coffee used (<strong>{bloom.maximum}g</strong>)
-				</li>
-				<li className="stage__item">Swirl the coffee slurry until evenly mixed</li>
-				<li className="stage__item stage__item--info">The aim is to wet all the coffee grounds by evenly mixing bloom water and coffee</li>
-				<li className="stage__item">Bloom for up to 45s </li>
-				<li className="stage__item stage__item--info">This allows CO2 to escape which will improve extraction</li>
-			</ul>
-		</div>
-	);
+	return StageOutput(stage);
 };
